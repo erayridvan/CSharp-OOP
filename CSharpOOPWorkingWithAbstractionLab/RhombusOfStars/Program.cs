479bb3cb8f57502c89eff7d47b0b5257ab22b8f4
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-10-30 18:02:32 +0200

    C# OOP

---
+++ CSharpOOPWorkingWithAbstractionLab/RhombusOfStars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace RhombusOfStars
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                sb.Append(new string(' ',n-i));
+
+                for (int k = 0; k < i; k++)
+                {
+                    sb.Append('*');
+                    sb.Append(' ');
+                }
+                sb.AppendLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                sb.Append(new string(' ', n - i));
+
+                for (int k = 0; k < i; k++)
+                {
+                    sb.Append('*');
+                    sb.Append(' ');
+                }
+                sb.AppendLine();
+
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
