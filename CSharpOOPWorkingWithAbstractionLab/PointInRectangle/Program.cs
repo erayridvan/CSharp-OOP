f593678fc7d9f490e5d28ff2ce0aa9f7b93d2e22
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-10-30 18:42:37 +0200

    C# OOP

--- CSharpOOPWorkingWithAbstractionLab/PointInRectangle/Program.cs
+++ CSharpOOPWorkingWithAbstractionLab/PointInRectangle/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Linq;
 
 namespace PointInRectangle
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] coordinates = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int topLeftX = coordinates[0];
+            int topLeftY = coordinates[1];
+            int bottomRightX = coordinates[2];
+            int bottomRightY = coordinates[3];
+
+            var rectangle = new Rectangle(topLeftY,topLeftX, bottomRightY, bottomRightX);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] currentPoint = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+                int firstCoordinate = currentPoint[0];
+                int secondCoordinate = currentPoint[1];
+
+                bool check = rectangle.Contains(new Point(firstCoordinate,secondCoordinate));
+
+                Console.WriteLine(check);
+            }
         }
     }
 }
---
+++ CSharpOOPWorkingWithAbstractionLab/PointInRectangle/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿namespace PointInRectangle
+{
+    public class Rectangle
+    {
+        public Rectangle(int top, int left, int bottom, int right)
+        {
+            this.TopLeft = new Point(left, top);
+            this.BottomRight = new Point(right, bottom); 
+        }
+
+        public Point TopLeft { get; set; }
+
+        public Point BottomRight { get; set; }
+
+        public bool Contains(Point point)
+        {
+            if (point.X>=TopLeft.X && point.X<=BottomRight.X &&
+                point.Y<=TopLeft.Y && point.Y>=BottomRight.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
