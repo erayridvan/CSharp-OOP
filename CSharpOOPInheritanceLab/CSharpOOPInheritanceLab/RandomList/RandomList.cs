6dc8aaad40c686ff13f43c78b36f2a9dbc87a064
erayridvan <52295311+erayridvan@users.noreply.github.com>
2019-11-06 14:03:43 +0200

    C# OOP

---
+++ CSharpOOPInheritanceLab/CSharpOOPInheritanceLab/RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿ using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+
+        public string RandomString(RandomList vs)
+        {
+            int currentWord = random.Next(vs.Count);
+
+            return vs[currentWord];
+        }
+
+        public void RemovesRandomElement(RandomList vs)
+        {
+            int currentWord = random.Next(vs.Count);
+
+            vs.Remove(vs[currentWord]);
+        }
+    }
+}
---
+++ CSharpOOPInheritanceLab/CSharpOOPInheritanceLab/StackОfStrings/StackofStrings1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace CustomStack
+{
+    public class StackStrings : Stack
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack AddRange(string[] elements, StackStrings stack)
+        {
+            foreach (var item in elements)
+            {
+                stack.Push(item);
+            }
+
+            return stack;
+        }
+
+    }
+}
